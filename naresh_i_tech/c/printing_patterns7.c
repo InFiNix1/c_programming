398af0b96cf50ffebc1c07b3d34b406f5b0f8e08
InFiNix1 <sauronildas10@gmail.com>
2020-03-02 23:20:24 +0530

    Printing Patterns

---
+++ naresh_i_tech/c/printing_patterns7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i, j, k;
+    printf("\n");
+
+    for(i=5; i>=1; i--)
+    {
+        for(j=i; j>1; j--)
+        {
+            printf("   ");
+        }
+        for(k=5; k>=i; k--)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }   
+    return 0;
+}
