f1182b5800d80e892e1ddd0062223949973a7f2d
Sauronil Das <sauronildas10@gmail.com>
2020-04-09 00:25:59 +0530

    Data Structures

---
+++ naresh_i_tech/DATA_STRUCTURES/stack/if.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int var;
+    scanf("%d", &var);
+    if(var)
+    {
+        printf("\nTrue\n");
+    }
+    else
+    {
+        printf("\nFalse\n");
+    }
+    return 0;
+}
